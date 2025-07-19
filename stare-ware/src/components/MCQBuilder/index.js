12725cc6d29580c3bf8fb29c8cb64efebc0e3a00
as6769-2004 <adityasahar9991@gmail.com>
2025-07-19 23:39:33 +0530

    Add MCQBuilder index file for exports

---
+++ stare-ware/src/components/MCQBuilder/index.js
@@ -0,0 +1,4 @@
+export { default as MCQTestForm } from './MCQTestForm';
+export { default as MCQQuestionList } from './MCQQuestionList';
+export { default as MCQQuestionForm } from './MCQQuestionForm';
+export { default as MCQOptionInput } from './MCQOptionInput'; 
