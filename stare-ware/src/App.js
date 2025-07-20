d1e8041577bd32d71bb9ba6617696302796928a7
as6769-2004 <adityasahar9991@gmail.com>
2025-07-20 18:07:44 +0530

    Update App.js: UI and routing improvements

--- stare-ware/src/App.js
+++ stare-ware/src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Dashboard from "./components/Dashboard";
 import CreateTest from "./pages/CreateTest";
@@ -9,6 +9,7 @@ import TopNav from "./components/TopNav";
 
 function App() {
   const [darkMode, setDarkMode] = useState(() => localStorage.getItem('darkMode') === 'true');
+  const location = useLocation();
 
   // Dark mode toggle
   useEffect(() => {
@@ -21,19 +22,26 @@ function App() {
     }
   }, [darkMode]);
 
+  // Only show TopNav if not on /appear-test
+  const showTopNav = !location.pathname.startsWith('/appear-test');
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-100 via-white to-purple-100 dark:from-gray-900 dark:via-gray-950 dark:to-gray-900 font-sans transition-colors">
-      <BrowserRouter>
-        <TopNav darkMode={darkMode} setDarkMode={setDarkMode} />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/create-test" element={<CreateTest />} />
-          <Route path="/appear-test" element={<AppearTest />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </BrowserRouter>
+      {showTopNav && <TopNav darkMode={darkMode} setDarkMode={setDarkMode} />}
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/create-test" element={<CreateTest />} />
+        <Route path="/appear-test" element={<AppearTest />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+export default function AppWithRouter() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
